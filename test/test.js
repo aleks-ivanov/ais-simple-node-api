547c0afab6f0b2366f0c5ea9ebd7be4ba0dab460
AleksIvanovSinglet <Aleksandar.Ivanov@singlet.dev>
2021-08-17 14:41:47 +0300

    set test to new endpoint

--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
 const request = require("supertest");
 const app = require("../index");
 
+let profile = {
+	name: 'admin',
+	email: 'admin@admin.com',
+	password: '123456'
+};
+
 describe("GET /", () => {
     it("responds with Hello World!", (done) => {
-        request(app).get("/").expect("Hello World!", done);
+        request(app).get("/").expect(profile, done);
     })
 });
