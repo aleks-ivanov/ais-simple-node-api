ea88c6ad859e88ae712950ab917527dad5e0d4dd
AleksIvanovSinglet <Aleksandar.Ivanov@singlet.dev>
2021-08-17 14:40:57 +0300

    set endpoint to profile object

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const port = process.env.PORT || 8000;
 const app = express();
 
+let profile = {
+	name: 'admin',
+	email: 'admin@admin.com',
+	password: '123456'
+};
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send(profile);
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!'); });
